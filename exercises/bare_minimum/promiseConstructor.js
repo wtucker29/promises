4fd0a01b32ceb367482f4f83dc849155d4ab7483
William Tucker <william.tucker29@gmail.com>
2024-02-13 11:38:19 -0600

    Pass promiseConstructor bare minimum

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -10,11 +10,29 @@ var Promise = require('bluebird');
 
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        var firstLine = data.split('\n')[0];
+        resolve(firstLine);
+      }
+    });
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
+  return new Promise((resolve, reject) => {
+    request.get(url, (err, response) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(response.statusCode);
+      }
+    });
+  });
   // TODO
 };
 
